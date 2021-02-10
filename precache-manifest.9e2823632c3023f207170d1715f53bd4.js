fe5bb1bfc9b08e68d52a43d1b2ab8b772b4cc42b
nakaosensei <nakaosensei@gmail.com>
2021-02-10 18:19:40 -0300

    Updates

---
+++ precache-manifest.9e2823632c3023f207170d1715f53bd4.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4329783ea7d3ada3d12c419da36027f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "23b546487550649a842e",
+    "url": "/static/css/2.fefb9901.chunk.css"
+  },
+  {
+    "revision": "a965f88cccd4d3464830",
+    "url": "/static/css/main.0978a44e.chunk.css"
+  },
+  {
+    "revision": "23b546487550649a842e",
+    "url": "/static/js/2.d2219d87.chunk.js"
+  },
+  {
+    "revision": "e204baec3ccd86bb774d2f311b5b982e",
+    "url": "/static/js/2.d2219d87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a965f88cccd4d3464830",
+    "url": "/static/js/main.14377472.chunk.js"
+  },
+  {
+    "revision": "60101dfb68b56482eab7",
+    "url": "/static/js/runtime-main.58caf1d4.js"
+  },
+  {
+    "revision": "7565403964fb40eb4bd4bd81f6a69fd3",
+    "url": "/static/media/claude3.75654039.jpg"
+  },
+  {
+    "revision": "129d4fcb93aa764f10feba771693e522",
+    "url": "/static/media/claude7.129d4fcb.jpg"
+  },
+  {
+    "revision": "78e8c257ef722b4bbaec9c55686d51ef",
+    "url": "/static/media/claude9.78e8c257.jpg"
+  },
+  {
+    "revision": "78a14c757a2389cae8febb39e2bc5de3",
+    "url": "/static/media/claudeTrain.78a14c75.jpg"
+  },
+  {
+    "revision": "b954b1372ba154220cd54cc8b94970d4",
+    "url": "/static/media/commanderDecks.b954b137.png"
+  },
+  {
+    "revision": "e8b9a2f1a97841c65bcea6573963a553",
+    "url": "/static/media/helpOthers.e8b9a2f1.png"
+  },
+  {
+    "revision": "b6458dd6906d743d9c6b60cd691b8f93",
+    "url": "/static/media/money0.b6458dd6.png"
+  },
+  {
+    "revision": "c4fa2157d5af193adf045dbc32fbb193",
+    "url": "/static/media/mtg3.c4fa2157.jpg"
+  },
+  {
+    "revision": "429c3aab5639df993d780278106eeda2",
+    "url": "/static/media/otherLists0.429c3aab.png"
+  },
+  {
+    "revision": "1c6f414e65e18f37af1c62c0efb6fe10",
+    "url": "/static/media/spell0.1c6f414e.png"
+  },
+  {
+    "revision": "0dd5a6d2420c399a925da64918bcbc2a",
+    "url": "/static/media/spellbook0.0dd5a6d2.png"
+  },
+  {
+    "revision": "fc9c828a2e5603a8b770b7198cb2e58b",
+    "url": "/static/media/testDecks.fc9c828a.png"
+  },
+  {
+    "revision": "29ab72283e1b4c7e9071f4921d6eadc4",
+    "url": "/static/media/testDecks0.29ab7228.png"
+  }
+]);
